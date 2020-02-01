7f3717a0d9a961fe224332e3504c433922dc446b
Corentin Grall <corentin.grall@gmail.com>
2020-02-01 15:36:57 +0100

    ravel residence fetching

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+process.env.NTBA_FIX_319 = 1
+
 const cheerio = require('cheerio')
-const fetch = require('fetch')
+const fetch = require('node-fetch')
 const TelegramBot = require('node-telegram-bot-api')
 
 const dotenv = require('dotenv')
@@ -12,11 +14,25 @@ if (!token) {
   throw new Error('Missing Telegram Token. Please provid a valid in .env file.')
 }
 
-const bot = new TelegramBot(token, { polling: true })
+const bot = new TelegramBot(token)
 bot.sendMessage(channel, "I'm still here and looking for an appartment!")
 
 // Matches "/av"
 bot.onText(/\/av/, (msg) => {
   const chatId = msg.chat.id
-  bot.sendMessage(chatId, 'Fetching avaibilities...')
+  bot.sendMessage(chatId, 'Fetching availability...')
 })
+
+const getRavel = async () => {
+  const fetched = await fetch('http://ravelresidence.studentexperience.nl/?language=en')
+  const body = await fetched.text()
+  const $ = cheerio.load(body)
+  const availabilities = []
+  const grounds = $('.home_available_element')
+  grounds.each((index, object) => {
+    availabilities.push($(object).text().replace(/(\r\n|\n|\r)/gm, '').trim().indexOf('No appartment'))
+  })
+  return availabilities
+}
+
+getRavel().then((ravel) => console.log(ravel))
